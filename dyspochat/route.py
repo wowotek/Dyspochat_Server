39adbe4ffceda1d407fbae658598549bb2d33475
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-07-07 02:38:36 +0700

    server quick fix. this message gonna pop up enough

--- dyspochat/route.py
+++ dyspochat/route.py
@@ -83,7 +83,7 @@ def chat_create():
             return json_response(
                 act_status=False,
                 data={},
-                cause=""
+                cause=chat[1]
             )
 
 @app.route("/chat/join", methods=["POST"])
