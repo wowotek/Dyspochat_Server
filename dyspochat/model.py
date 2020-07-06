d119af3e00805a92835aaaf657fdcc96cbf2b324
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-07-07 02:44:32 +0700

    server quick fix. this message gonna pop up enough

--- dyspochat/model.py
+++ dyspochat/model.py
@@ -8,7 +8,7 @@ from . import db
 class ChatRecipient(db.Model):
     __tablename__ = 'chat_recipients'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     chatroom_id = db.Column(db.ForeignKey('chatroom.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True, nullable=False, unique=True)
     user_id = db.Column(db.ForeignKey('user.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True, nullable=False, unique=True)
 
@@ -20,7 +20,7 @@ class ChatRecipient(db.Model):
 class Chatroom(db.Model):
     __tablename__ = 'chatroom'
 
-    id = db.Column(db.Integer, primary_key=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     room_id = db.Column(db.String(45), nullable=False, unique=True)
 
 
@@ -28,7 +28,7 @@ class Chatroom(db.Model):
 class Message(db.Model):
     __tablename__ = 'message'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     user_id = db.Column(db.ForeignKey('user.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True, nullable=False, unique=True)
     chatroom_id = db.Column(db.ForeignKey('chatroom.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True, nullable=False, unique=True)
     content = db.Column(db.Text)
@@ -41,6 +41,6 @@ class Message(db.Model):
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     username = db.Column(db.String(32), nullable=False, unique=True)
     password = db.Column(db.String(512), nullable=False)
