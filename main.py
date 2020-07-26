fb3da19373a6bccd52c2d77dde252e5f26f010bb
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-07-26 12:46:26 +0700

    updated wsgi configuration

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     print()
     print("--- Starting Server ---")
     print()
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=80)
--- server/__init__.py
+++ server/__init__.py
@@ -5,13 +5,11 @@ from flask_cors import CORS
 from .database import Database
 
 app = Flask(__name__)
-
 FlaskJSON(app)
 CORS(app, resources={r'/*': {'origins': '*'}})
+db = Database()
 
 # TODO: ENGINE/SSL: create properly Signed SSL key and certificates
 # TODO: ENGINE/SSL: implement SSL Context
-
-db = Database()
 
 from . import routes
