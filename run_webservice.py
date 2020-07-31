9f8ee727bb14945d3fc0c70ec01c476dfb5cc836
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-08-01 02:39:14 +0700

    typo

--- run_webservice.py
+++ run_webservice.py
@@ -6,4 +6,4 @@ from webservice import app
 if __name__ == '__main__':
     time.sleep(0.5)
     print("--- Starting Server ---")
-    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=("/cert/letsencrypt/live/dyspochat.com/cert.pem", "/cert/letsencrypt/live/dyspochat.com/privkey.pem"))
+    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=("/etc/letsencrypt/live/dyspochat.com/cert.pem", "/etc/letsencrypt/live/dyspochat.com/privkey.pem"))
