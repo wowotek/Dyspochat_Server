160d23cb7dc9b29dc44692e2e79832fa13b9e6a7
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-08-06 16:46:51 +0700

    testing different umask configuration

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
 bind = 'unix:dyspochat_webservice.sock'
-umask = 0o007
+umask = 0o666
 reload = True
 
 accesslog = "access.log"
