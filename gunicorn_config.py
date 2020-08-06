be6cb41aef9ddfde40bd695a36d8017780a72f5b
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-08-06 17:18:26 +0700

    experiment with sock permissions

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
 bind = 'unix:dyspochat_webservice.sock'
-umask = 0o666
+umask = 0o777
 reload = True
 
 accesslog = "access.log"
