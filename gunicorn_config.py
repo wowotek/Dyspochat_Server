853a30e6dfb7edccefed7d427422dc55662bec3c
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-08-06 17:26:26 +0700

    changed permission for socket

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
 bind = 'unix:dyspochat_webservice.sock'
-umask = 0o777
+umask = 0o0777
 reload = True
 
 accesslog = "access.log"
